5e82a9bc32c0528675f73c82eff487c0a33d4bfe
Vladyslav <vados1789@gmail.com>
2024-02-12 20:57:55 +0100

    Add picture to conference

--- Frontend/src/app/conference/page.tsx
+++ Frontend/src/app/conference/page.tsx
@@ -1,15 +1,37 @@
 import Link from "next/link";
+import Image from "next/image";
+import ConferencePic from "../images/nature3.jpg"
+import styles from "./conference.module.css";
 
 export default function Page() {
     return (
         <>
-            <h1>Welcome to Conference</h1>
-            <h2>
-                <Link href="/conference/sessions">Sessions</Link>
-            </h2>
-            <h2>
-                <Link href="/conference/speakers">Speakers</Link>
-            </h2>
+            <div className={styles.bgWrap}>
+                <Image
+                    src={ConferencePic}
+                    alt="Our conference picture"
+                    quality={100}
+                    placeholder="blur"                   
+                    sizes={"100vw"}
+                    fill
+                    style={{
+                        objectFit: "cover"
+                    }}
+                />
+                <div className={styles.overlay}>
+                <h1 className={styles.bgHeader}>Welcome to Conference</h1>
+                <h2 className={styles.bgWrap}>
+                    <Link href="/conference/sessions">
+                        Sessions
+                    </Link>
+                </h2>
+                <h2 className={styles.bgWrap}>
+                    <Link href="/conference/speakers">
+                        Speakers
+                    </Link>
+                </h2>
+                </div>
+            </div>        
         </>
     );
 }
